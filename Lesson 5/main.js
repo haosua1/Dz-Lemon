2de53e7b7028cd9300006d421c3bc3ca352400e0
haosua1 <63312161+haosua1@users.noreply.github.com>
2020-06-01 22:22:51 +0300

    Add files via upload

---
+++ Lesson 5/main.js
@@ -0,0 +1,117 @@
+/*
+// let user = {
+//   name: "Aleksandr",
+//   surName: "Karnaulskyi",
+//   getName() {
+//     return this.name;
+//   },
+
+//   getFullname: function () {
+//     return user.name + "" + user.surName;
+//   },
+// };
+
+// console.log(user.getName());
+
+//Обекты
+
+// let calkulator = {
+//   plus: 100 + 100,
+//   minus: 100 - 50,
+//   mnozit: 4 * 5,
+//   delit: 10 / 5,
+// };
+// console.log(calkulator.plus);
+
+// user.getSurName = function () {
+//   return user.suername;
+// };
+
+// let a = 100;
+// let b = "mndsm";
+// let c = a;
+
+// let user1 = user;
+
+// user1.age = 33;
+
+// let user = {
+//   age: 33,
+//   age2: 3,
+//   age3: 353,
+//   age4: 3663,
+//   age5: 363,
+// };
+// user.age = 100;
+
+// // console.log(age in user);
+
+// for (const key in user) {
+//   console.log(user[key]);
+// }
+
+//масссывы
+
+// let arr = [33, 44];
+
+// console.log(arr);
+
+// arr.join();
+// arr.push(10);
+// console.log(arr);
+
+let data = {
+  mesaits: "aprel",
+  vremiagoda: "vestna",
+  dneiVMesiatse: [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+  ],
+  pogoda: "govno",
+  den: "18/04/2020",
+};
+
+console.log(data.pogoda);
+
+let num = prompt("tsifra?");
+
+if (num == 1) {
+  console.log(data.vremiagoda);
+}
+
+if (num == 2) {
+  console.log(data.mesaits);
+}
+
+if (num == 3) {
+  console.log(data.den);
+}
+*/
