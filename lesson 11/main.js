5a7faaf890cbd809a1d0f4087e7ff2824a185af6
haosua1 <63312161+haosua1@users.noreply.github.com>
2020-06-01 09:25:43 +0300

    Add files via upload

---
+++ lesson 11/main.js
@@ -0,0 +1,68 @@
+let a = 100;
+console.log(a);
+
+//ООП
+
+// let user = {
+//   age: 100,
+//   hight: 182,
+// };
+
+// console.log(user.age, user.hight);
+
+//обьект
+
+// let user = {
+//   name: "Sasha",
+//   arr: [23, 32, 33, 56],
+//   getName() {
+//     return "Sasha2";
+//   },
+//   country: { city: "Kharkiv", country: "Ukraine" },
+// };
+
+// let dog2 = {
+//   legs: { frontRightLeg: 1, frontleftLeg: 2, backRightLeg: 3, backleftLeg: 4 },
+//   goodBoy() {
+//     return console.log("good one");
+//   },
+// };
+// console.log(dog2.goodBoy);
+
+class Animal {
+  constructor(name, age, legs) {
+    (this.age = age), (this.name = name), (this.legs = legs);
+  }
+  getVoice() {
+    console.log(this.name + " says: Gav");
+  }
+}
+
+class Cat extends Animal {}
+
+let dog = new Animal("Zuzick", 3, 4);
+
+console.log(dog);
+
+let obj = {
+  name: "Sasha",
+  getName() {
+    return this.name();
+  },
+};
+console.log(obj.getName);
+
+let cat = new Animal("Murchik", 4, 4);
+console.log(cat);
+
+//создать иерархию классов
+
+class ziteli {
+  constructor(monkey) {
+    this.monkey;
+  }
+}
+let ziteli = new monkey(age);
+let ziteli = new dog(age);
+let ziteli = new cat(age);
+let ziteli = new people(age);
---
+++ lesson 9/main.js
@@ -0,0 +1,34 @@
+let h1 = document.querySelector("h1");
+// h1.onclick = function name(params) {
+//   alert("hello");
+// };
+
+// h1.addEventListener("click", function name(params) {
+//   alert("good day!");
+// });
+
+// let p = document.querySelector("p");
+// p.dblclick = function name(params) {
+//   alert("hello");
+// };
+
+// p.addEventListener("dblclick", function name(params) {
+//   alert("good day2!");
+// });
+
+//прерывание
+
+// h1.addEventListener();
+
+// let p = document.querySelector("p");
+// h1.onclick = function name(params) {
+//   alert("hello2");
+//   p.stopPropagation;
+// };
+
+let ul = document.querySelector("ul");
+
+ul.addEventListener("click", function (event) {
+  let li = event.target;
+  alert(li.innerText);
+});
